44ae0f0a661ddee5d7c16476be2e457927483307
HeleneB <helene.blanche@hotmail.fr>
2024-11-04 17:22:06 +0100

    New step of the blog project

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<str:pk>/', views.post, name='post'),
+    path('create/', views.add_post, name='create'),
+    path('post/<int:pk>/delete/', views.delete_post, name='delete_post'),
+    path('post/<int:pk>/update/', views.update_post, name='update_post')
+    
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from django.http import HttpResponse
+from django.contrib import messages
+
+
 
 # Create your views here.
 
@@ -13,3 +17,46 @@ def index(request):
 def post(request, pk):
      posts = Post.objects.get(id=pk)
      return render(request, 'posts.html', {'posts':posts}) 
+
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+
+        return redirect('index')
+
+    return render(request, 'create.html', {'message': 'Post added successfully!'})
+    
+def update_post(request, pk):
+    """Met à jour un post existant."""
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        
+        if not title or not content:
+            messages.error(request, 'Title and content are required.')
+            return render(request, 'update_post.html', {'post': post})
+            
+        post.title = title
+        post.content = content
+        post.save()
+        
+        messages.success(request, 'Post updated successfully!')
+        return redirect('post', pk=post.pk)
+        
+    return render(request, 'update_post.html', {'post': post})
+# def delete_post(request, pk):
+#     post = Post.objects.get(id=pk)
+#     post.delete()
+#     return HttpResponse('Post deleted successfully!')
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, 'Post deleted successfully!')  # Message de succès
+        return redirect('index')
+    return render(request, 'delete_post.html', {'post': post})
